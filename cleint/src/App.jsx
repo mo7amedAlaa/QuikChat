7632665827cf9709d38c2523ebdcd9d0a79c8cce
mo7amedAlaa <mohamedalhakar5@gmail.com>
2025-08-22 05:12:17 +0300

    server setup and asset folder

--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const {authUser} = useContext(AuthContext);
   console.log(`Auth user: ${authUser}`);
   return (
-    <div className="bg-[url('./assets/bgImage.svg')]  bg-contain  ">
+    <div className="bg-[url('/bgImage.svg')]  bg-contain  ">
       <Toaster />
      <Routes>
         <Route path="/" element={authUser ?   <HomePage /> :<Navigate to="/login" />} />
--- server/server.js
+++ server/server.js
@@ -97,7 +97,9 @@ app.use("/api/messages", messageRouter);
 // ========================
 // Start Server
 // ========================
-server.listen(PORT, () => {
-  connectToDB();
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  server.listen(PORT, () => {
+    connectToDB();
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+}
